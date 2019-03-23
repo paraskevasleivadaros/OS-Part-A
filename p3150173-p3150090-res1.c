9d237700b5c4f3ba8c7e99d4af79ff283adb180a
paraskevasleivadaros <paraskevasleivadaros@gmail.com>
2019-03-23 21:34:36 +0200

    ncust and seeds

--- p3150173-p3150090-res1.c
+++ p3150173-p3150090-res1.c
@@ -1,9 +1,27 @@
 //
 // Created by paras on 23-Mar-19.
 //
-#include <stdio.h>
 #include "p3150173-p3150090-res1.h"
 
-int main(){
-    printf("Hello World");
+double profit = 0.0;
+int transcations = 0;
+
+int main(int argc, char* argv[]){
+
+    // Converting string type to integer type
+    // using function atoi
+    int ncust = strtol(argv[1]);
+    int seed = strtol(argv[2]);
+
+    // Checking if all provided numbers are positive or not
+    if (ncust < 0 || seed < 0){
+        printf("Please enter only positive values in arguments!");
+        exit(1);
+    }
+
+    // TODO
+    //
+
+    printf("customers = %d, seed = %d", ncust, seed);
+
 }
--- p3150173-p3150090-res1.h
+++ p3150173-p3150090-res1.h
@@ -6,3 +6,16 @@
 #define POSIX_THREADS_P3150173_P3150090_RES1_H
 
 #endif //POSIX_THREADS_P3150173_P3150090_RES1_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N_SEATS 250
+#define N_TEL 8
+#define N_SEAT_LOW 1
+#define N_SEAT_HIGH 5
+#define T_SEAT_LOW 5
+#define T_SEAT_HIGH 10
+#define P_CARD_SUCCESS 0.9
+#define C_SEAT 20
+
